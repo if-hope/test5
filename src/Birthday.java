5efb3d8fb9ee4dfecf668680547d30b063c441a8
Nadia Yef <tester17hi@gmail.com>
2023-04-02 05:22:14 +0200

    homework5

---
+++ src/Birthday.java
@@ -0,0 +1,11 @@
+public class Birthday {
+
+    int age = 35;
+
+    public int printAge(){
+
+        System.out.println("---" + age + "---");
+
+        return age;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+            Birthday birthday = new Birthday();
+
+            int result = birthday.printAge();
+
+            System.out.println("~~~" + result + "~~~");
+
+        }
+
+    }
+
+
