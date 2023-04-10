14323781f6b6bc2f550af41fbae510e19a243716
Nadia Yef <tester17hi@gmail.com>
2023-04-10 16:17:51 +0200

    uncommented method

--- src/Main.java
+++ src/Main.java
@@ -10,35 +10,35 @@ public class Main {
 
 
 
-//        int day = 8;
-//         switch (day) {
-//             default:
-//                 System.out.println("No such day in week");
-//                 //break;
-//             case 1:
-//                 System.out.println("Sunday");
-//                 break;
-//             case 2:
-//                 System.out.println("Monday");
-//                 break;
-//             case 3:
-//                 System.out.println("Tuesday");
-//                 break;
-//             case 4:
-//                 System.out.println("Wednesday");
-//                 break;
-//             case 5:
-//                 System.out.println("Thursday");
-//                 break;
-//             case 6:
-//                 System.out.println("Friday");
-//                 break;
-//             case 7:
-//                 System.out.println("Saturday");
-//                 break;
-//             //default:
-//                 //System.out.println("No such day in week");
-//         }
+       int day = 8;
+         switch (day) {
+             default:
+                 System.out.println("No such day in week");
+                 //break;
+             case 1:
+                 System.out.println("Sunday");
+                 break;
+             case 2:
+                 System.out.println("Monday");
+                 break;
+             case 3:
+                 System.out.println("Tuesday");
+                 break;
+             case 4:
+                 System.out.println("Wednesday");
+                 break;
+             case 5:
+                 System.out.println("Thursday");
+                 break;
+             case 6:
+                 System.out.println("Friday");
+                 break;
+             case 7:
+                 System.out.println("Saturday");
+                 break;
+             //default:
+                 //System.out.println("No such day in week");
+         }
 
 //         String text = "Hello";
 //
